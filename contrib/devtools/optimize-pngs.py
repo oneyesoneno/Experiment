fb5e6599a04c14a821626ffbf3c7632006b53da5
oneyesoneno <oneyesoneno@gmail.com>
2017-03-01 17:48:17 +0700

    Uh Yeah Uh No
    Uh Yeah Uh No

--- contrib/devtools/optimize-pngs.py
+++ contrib/devtools/optimize-pngs.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 '''
 Run this script every time you change one of the png files. Using pngcrush, it will optimize the png files, remove various color profiles, remove ancillary chunks (alla) and text chunks (text).
-#pngcrush -brute -ow -rem gAMA -rem cHRM -rem iCCP -rem sRGB -rem alla -rem text.
+#pngcrush -brute -ow -rem gAMA -rem cHRM -rem iCCP -rem sRGB -rem alla -rem text
 '''
 import os
 import sys
